0af1e109b2d0d810fb378f89c01ce1bad323d0f5
leonaminacio <leonamalves46@gmail.com>
2021-05-28 16:21:06 -0300

    corrigindo

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
 let nome = "Leonam"
+if (idade >= 18){
+    console.log("Ae você é maior de idade")
+}else{
+    console.log("Você não é maior de idade")
+}
+
+
+
 let idade = 18
-let sobrenome = "Alves"
-let mês = "Agosto"
 let humano = true
 
-if (idade >= 18 || humano){
+if((idade >= 18) && (humano == true)){
     console.log("Ae você é maior de idade e você é humano")
 }else{
     console.log("Você não é maior de idade e você não é humano")
 }
 
-if (mês == "Janeiro" || mês == "Dezembro"){
+
+let mês = "Agosto"
+
+
+if(mês === "Janeiro" || mês === "Dezembro"){
     console.log ("Você faz aniversário nas férias de fim de ano")
 }else{
     console.log("Você não faz aniversário nas férias de fim de ano" )
 }
 
-if (sobrenome.length > 6 || nome.substring(0,1) == "E"){
-console.log("Seu sobrenome tem mais de 6 letras e seu nome começa com a letra E")
+
+let nome = "Leonam"
+
+if(nome.substring(0,1) == "R"){
+console.log("Seu nome começa com a letra E")
 }else{
-    console.log("Seu sobrenome tem menos de 6 letras e seu nome não começa com a letra E")
+    console.log("Seu nome não começa com a letra E")
 }
 
 
+
+
+let nome = "Leonam"
+let sobrenome = "Alves"
+
+
+
+if (sobrenome.length > 6 || nome.substring(0,1) == "E"){
+    console.log("Seu sobrenome tem mais de 6 letras ou seu nome começa com a letra E")
+    }else{
+        console.log("Seu sobrenome tem menos de 6 letras ou seu nome não começa com a letra E")
+    }
 
